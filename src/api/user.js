5e8a9ae55ec0374be02a0174246714e60ec296a6
zuozuokan <14971069+zuozuokan@user.noreply.gitee.com>
2025-06-27 16:05:18 +0800

    新增用户个人信息管理

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,46 @@
 import request from "../util/userRequest";
 
 export const userGetCurrentInfoAPI = (uuid) =>
-  request.post("/get-current-user-info", null,{
+  request.post("/get-current-user-info", null, {
     params: {
-      uuid: uuid
-    }
+      uuid: uuid,
+    },
+  });
+
+export const userUpdatePasswordAPI = (uuid, newPassword) =>
+  request.post("/update-password", null, {
+    params: {
+      uuid: uuid,
+      newPassword: newPassword,
+    },
+  });
+
+export const userUpdateNameAPI = (uuid, nickname) =>
+  request.post("/update-name", null, {
+    params: {
+      uuid: uuid,
+      nickname: nickname,
+    },
+  });
+
+export const userUpdatePhoneAPI = (uuid, phone) =>
+  request.post("/update-phone", null, {
+    params: {
+      uuid: uuid,
+      phone: phone,
+    },
+  });
+
+export const userAddAddressAPI = (uuid, address) =>
+  request.post("/add-address", address, {
+    params: {
+      uuid: uuid,
+    },
+  });
+
+export const userUpdateAddressAPI = (addressUuid, address) =>
+  request.post("/update-address", address, {
+    params: {
+      addressUuid: addressUuid,
+    },
   });
--- src/router/index.js
+++ src/router/index.js
@@ -11,15 +11,23 @@ import UserManagement from "@/views/UserManagement.vue";
 import ExpertManagement from "@/views/ExpertManagement.vue";
 import BankManagement from "@/views/BankManagement.vue";
 import AdminManagement from "@/views/AdminManagement.vue";
+import VisitorPage from "@/views/VisitorPage.vue"; // 确保导入 VisitorPage 组件
+import UserInfoExchange from "@/views/UserInfoExchange.vue";
+import AddProduct from "@/views/AddProduct.vue";
+import ProductManage from "@/views/ProductManage.vue";
+import AddFinancing from "@/views/AddFinancing.vue";
+import FinancingList from "@/views/FinancingList.vue";
+import AddKnowledge from "@/views/AddKnowledge.vue";
+import UserKnowledge from "@/views/UserKnowledge.vue";
 
 
 const routes = [
   {
     path: "/",
-    name: "userLogin",
+    name: "visitorPage",
     component: userLogin,
     meta: {
-      title: "用户登录",
+      title: "访客页面",
       hideSidebar: true, // 隐藏侧边栏
     },
   },
@@ -120,6 +128,78 @@ const routes = [
     component: AdminManagement,
     meta: {
       title: "管理员后台管理",
+    },
+  },
+  {
+    path: "/user-info-exchange",
+    name: "userInfoExchange",
+    component: UserInfoExchange,
+    meta: {
+      title: "用户信息更改",
+      hideSidebar: true, 
+    },
+  },
+  {
+    path: "/user-info-exchange",
+    name: "userInfoExchange",
+    component: UserInfoExchange,
+    meta: {
+      title: "用户信息更改",
+      hideSidebar: true, 
+    },
+  },
+  {
+    path: "/add-product",
+    name: "addProduct",
+    component: AddProduct,
+    meta: {
+      title: "添加产品",
+      hideSidebar: true, 
+    },
+  },
+  {
+    path: "/product-manage",
+    name: "productManage",
+    component: ProductManage,
+    meta: {
+      title: "产品管理",
+      hideSidebar: true, 
+    },
+  },
+  {
+    path: "/add-financing",
+    name: "addFinancing",
+    component: AddFinancing,
+    meta: {
+      title: "发起融资",
+      hideSidebar: true, 
+    },
+  },
+  {
+    path: "/financing-list",
+    name: "financingList",
+    component: FinancingList,
+    meta: {
+      title: "融资列表",
+      hideSidebar: true, 
+    },
+  },
+  {
+    path: "/add-knowledge",
+    name: "addKnowledge",
+    component: AddKnowledge,
+    meta: {
+      title: "发布知识",
+      hideSidebar: true, 
+    },
+  },
+  {
+    path: "/user-knowledge",
+    name: "userKnowledge",
+    component: UserKnowledge,
+    meta: {
+      title: "我的知识管理",
+      hideSidebar: true, 
     },
   },
   
