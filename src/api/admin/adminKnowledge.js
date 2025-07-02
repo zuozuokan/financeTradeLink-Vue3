41439feecd0481c6fa1a33ce78fd3c86af3bb1ef
qyzy <wanghao1586839404@163.com>
2025-07-03 01:02:18 +0800

    ADD：管理员知识管理功能

--- src/api/admin/adminKnowledge.js
+++ src/api/admin/adminKnowledge.js
@@ -1,21 +1,23 @@
-import axios from 'axios';
-
-const adminRequest = axios.create({
-  baseURL: '/project-admin/api/admin',
-  timeout: 5000,
-});
+import request from "../../util/knowledgeRequest";
 
 // 获取所有知识内容
-export const getAllKnowledgeAPI = () => adminRequest.get('/find-all-knowlodge');
-
+export const getAllKnowledgeAPI = () => request.get('/list');
 // 新增知识内容
 export const addKnowledgeAPI = (knowledge) =>
-  adminRequest.post('/add-knowlodge', knowledge);
+  request.post('/add', knowledge);
 
-// 更新知识内容
-export const updateKnowledgeAPI = (knowledge) =>
-  adminRequest.post('/update-knowlodge', knowledge);
+// 通过知识的uuid删除知识
+export const deleteKnowledgeAPI = (uuid) => {
+  return request.post(`/del/${uuid}`, {
+    params: {
+      uuid,
+    },
+  });
+};
 
-// 删除知识内容
-export const deleteKnowledgeAPI = (knowledgeUuid) =>
-  adminRequest.post('/delete-knowlodge', new URLSearchParams({ knowledgeUuid }));
+// 更新知识
+export const updateKnowledgeAPI = (uuid, updatedData) => {
+  return request.post(`/update/${uuid}`, updatedData, {
+    params: { uuid },
+  });
+};
